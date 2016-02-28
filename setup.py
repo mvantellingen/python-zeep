6075dd074f29c296db2e577d610cb704471e17eb
Michael van Tellingen <michael@mvantellingen.nl>
2016-02-28 20:12:40 +0100

    Add lxml and requests to `install_requires`

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import find_packages, setup
 
+install_requires = [
+    'lxml>=3.0.0',
+    'requests>=2.7.0',
+]
 
 tests_require = [
     'pytest>=2.8.3',
@@ -9,15 +13,14 @@ tests_require = [
 setup(
     name='zeep',
     version='0.0.1',
-    description='',
+    description='SOAP Client',
     url='http://www.python-zeep.org',
     author="Michael van Tellingen",
     author_email="michaelvantellingen@gmail.com",
-    install_requires=[],
+    install_requires=install_requires,
     tests_require=tests_require,
     extras_require={'test': tests_require},
-    entry_points={
-    },
+    entry_points={},
     package_dir={'': 'src'},
     packages=find_packages('src'),
     include_package_data=True,
