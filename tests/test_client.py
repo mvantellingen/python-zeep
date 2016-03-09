8d912669361cd3b89e62b61dd82679a45b8338a6
Michael van Tellingen <michael@mvantellingen.nl>
2016-03-09 21:23:02 +0100

    Fix tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,19 @@
 from zeep import client
 
 
-def test_create_soap_message():
-    soap = client.create_soap_message()
+def test_get_port_default():
+    client_obj = client.Client('tests/wsdl_files/soap.wsdl')
+    port = client_obj.get_port()
+    assert port
+
+
+def test_get_port_service():
+    client_obj = client.Client('tests/wsdl_files/soap.wsdl')
+    port = client_obj.get_port('StockQuoteService')
+    assert port
+
+
+def test_get_port_service_port():
+    client_obj = client.Client('tests/wsdl_files/soap.wsdl')
+    port = client_obj.get_port('StockQuoteService', 'StockQuotePort')
+    assert port
--- tests/test_wsdl.py
+++ tests/test_wsdl.py
@@ -1,5 +1,6 @@
 from zeep import wsdl
 from zeep.transports import Transport
+import requests_mock
 
 
 def test_parse_wsdl():
@@ -15,11 +16,26 @@ def test_parse_wsdl():
 
     transport = Transport()
 
-    port.send(
-        transport=transport,
-        operation='{http://example.com/stockquote.wsdl}GetLastTradePrice',
-        args=[],
-        kwargs={'tickerSymbol': 'foobar'})
+    response = """
+    <?xml version="1.0"?>
+    <SOAP-ENV:Envelope
+      xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+      xmlns:s="http://example.com/stockquote.xsd">
+      <SOAP-ENV:Body>
+        <s:TradePrice>
+          <s:price>120.123</s:price>
+        </s:TradePrice>
+      </SOAP-ENV:Body>
+    </SOAP-ENV:Envelope>
+    """.strip()
+
+    with requests_mock.mock() as m:
+        m.post('http://example.com/stockquote', text=response)
+        result = port.send(
+            transport=transport,
+            operation='{http://example.com/stockquote.wsdl}GetLastTradePrice',
+            args=[],
+            kwargs={'tickerSymbol': 'foobar'})
+        assert result.price == 120.123
 
 
-    # print operation.input('John', 'Doe', 'j.doe@example.com')
