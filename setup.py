48068e5fa5e641ba55c68a9f17c23aea093abc01
Michael van Tellingen <michael@mvantellingen.nl>
2016-03-20 18:05:59 +0100

    Update setup.py classifiers

--- setup.py
+++ setup.py
@@ -37,6 +37,14 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     zip_safe=False,
 )
