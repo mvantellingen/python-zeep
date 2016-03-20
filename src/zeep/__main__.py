45824c806da94dbd07b9399f8539f6e04e98cf36
Michael van Tellingen <michael@mvantellingen.nl>
2016-03-20 21:10:34 +0100

    Use non-persistent cache for `python -mzeep` usecache

--- src/zeep/__main__.py
+++ src/zeep/__main__.py
@@ -4,11 +4,13 @@ import sys
 
 if __name__ == '__main__':
     from zeep.client import Client
+    from zeep.cache import SqliteCache
 
     if len(sys.argv) < 2:
         print("Missing argument. Please pass a WSDL", file=sys.stderr)
         sys.exit(1)
 
-    client = Client(sys.argv[1])
+    cache = SqliteCache(persistent=False)
+    client = Client(sys.argv[1], cache=cache)
     client.wsdl.dump()
     sys.exit(0)
