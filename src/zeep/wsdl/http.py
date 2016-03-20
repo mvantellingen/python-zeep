263fbc6dd9c6d3bae49d08918f3043426939d2fe
Michael van Tellingen <michael@mvantellingen.nl>
2016-03-20 19:18:53 +0100

    Implement basic http binding to fix errors when a wsdl contains both

--- src/zeep/wsdl/http.py
+++ src/zeep/wsdl/http.py
@@ -1,4 +1,5 @@
 from lxml import etree
+from zeep.utils import get_qname
 
 from zeep.wsdl.definitions import Binding
 
@@ -14,3 +15,22 @@ class HttpBinding(Binding):
     def match(cls, node):
         http_node = node.find(etree.QName(NSMAP['http'], 'binding'))
         return http_node is not None
+
+    @classmethod
+    def parse(cls, wsdl, xmlelement):
+        name = get_qname(
+            xmlelement, 'name', wsdl.target_namespace, as_text=False)
+        port_name = get_qname(xmlelement, 'type', wsdl.target_namespace)
+        port_type = wsdl.ports[port_name]
+
+        obj = cls(name, port_type)
+        return obj
+
+    def process_service_port(self, xmlelement):
+        address_node = xmlelement.find('http:address', namespaces=NSMAP)
+        if address_node is None:
+            raise ValueError("No `http:address` node found")
+
+        return {
+            'address': address_node.get('location')
+        }
