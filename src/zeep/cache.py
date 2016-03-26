1a3be733680fac4da50094b0c11bbd708012e1cc
Michael van Tellingen <michael@mvantellingen.nl>
2016-03-26 18:23:37 +0100

    base64 encode the cache data in the SqliteCache backend

--- src/zeep/cache.py
+++ src/zeep/cache.py
@@ -1,9 +1,12 @@
+import base64
 import datetime
 import os
 import sqlite3
 
 
 class SqliteCache(object):
+    _version = '1'
+
     def __init__(self, persistent=True, path=None, timeout=3600):
         self._timeout = timeout
 
@@ -24,11 +27,12 @@ class SqliteCache(object):
             """)
 
     def add(self, url, content):
+        data = self._encode_data(content)
         cursor = self._db.cursor()
         cursor.execute("DELETE FROM request WHERE url = ?", (url,))
         cursor.execute(
             "INSERT INTO request (created, url, content) VALUES (?, ?, ?)",
-            (datetime.datetime.utcnow(), url, content))
+            (datetime.datetime.utcnow(), url, data))
         self._db.commit()
 
     def get(self, url):
@@ -37,9 +41,21 @@ class SqliteCache(object):
             "SELECT created, content FROM request WHERE url=?", (url, ))
         rows = cursor.fetchall()
         if rows:
-            created, content = rows[0]
+            created, data = rows[0]
             offset = (
                 datetime.datetime.utcnow() -
                 datetime.timedelta(seconds=self._timeout))
             if not self._timeout or created > offset:
-                return content
+                return self._decode_data(data)
+
+    def _encode_data(self, data):
+        data = base64.b64encode(data)
+        return self._version_string + data
+
+    def _decode_data(self, data):
+        if data.startswith(self._version_string):
+            return base64.b64decode(data[len(self._version_string):])
+
+    @property
+    def _version_string(self):
+        return '$ZEEP:%s$' % self._version
