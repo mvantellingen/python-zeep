fc150d6f7c983ce9ee16467f29547ab75190a94a
Michael van Tellingen <michael@mvantellingen.nl>
2016-03-26 18:23:14 +0100

    Add all types to xsd_builtins and use correct inheritance tree

--- src/zeep/xsd/builtins.py
+++ src/zeep/xsd/builtins.py
@@ -1,27 +1,62 @@
+"""
+    Primitive datatypes
+     - string
+     - boolean
+     - decimal
+     - float
+     - double
+     - duration
+     - dateTime
+     - time
+     - date
+     - gYearMonth
+     - gYear
+     - gMonthDay
+     - gDay
+     - gMonth
+     - hexBinary
+     - base64Binary
+     - anyURI
+     - QName
+     - NOTATION
+
+    Derived datatypes
+     - normalizedString
+     - token
+     - language
+     - NMTOKEN
+     - NMTOKENS
+     - Name
+     - NCName
+     - ID
+     - IDREF
+     - IDREFS
+     - ENTITY
+     - ENTITIES
+     - integer
+     - nonPositiveInteger
+     - negativeInteger
+     - long
+     - int
+     - short
+     - byte
+     - nonNegativeInteger
+     - unsignedLong
+     - unsignedInt
+     - unsignedShort
+     - unsignedByte
+     - positiveInteger
+
+
+"""
+
 from decimal import Decimal as _Decimal
 
 from zeep.xsd.types import SimpleType
 
 
-class AnyType(SimpleType):
-    name = 'xsd:anyType'
-
-    def xmlvalue(self, value):
-        return value
-
-    def pythonvalue(self, value):
-        return value
-
-
-class AnyURI(SimpleType):
-    name = 'xsd:anyURI'
-
-    def xmlvalue(self, value):
-        return value
-
-    def pythonvalue(self, value):
-        return value
-
+##
+# Primitive types
 
 class String(SimpleType):
     name = 'xsd:string'
@@ -33,26 +68,6 @@ class String(SimpleType):
         return value
 
 
-class ID(String):
-    name = 'xsd:ID'
-
-
-class IDREF(ID):
-    name = 'xsd:IDREF'
-
-
-class Byte(String):
-    name = 'xsd:byte'
-
-
-class Base64Binary(String):
-    name = 'xsd:base64Binary'
-
-
-class QName(String):
-    name = 'xsd:QName'
-
-
 class Boolean(SimpleType):
     name = 'xsd:boolean'
 
@@ -60,14 +75,48 @@ class Boolean(SimpleType):
         return 'true' if value else 'false'
 
     def pythonvalue(self, value):
+        """Return True if the 'true' or '1'. 'false' and '0' are legal false
+        values, but we consider everything not true as false.
+
+        """
         return value in ('true', '1')
 
 
-class Date(SimpleType):
-    name = 'xsd:date'
+class Decimal(SimpleType):
+    name = 'xsd:decimal'
 
     def xmlvalue(self, value):
-        return value.strftime('%Y-%m-%d')
+        return str(value)
+
+    def pythonvalue(self, value):
+        return _Decimal(value)
+
+
+class Float(SimpleType):
+    name = 'xsd:float'
+
+    def xmlvalue(self, value):
+        return str(value).upper()
+
+    def pythonvalue(self, value):
+        return float(value)
+
+
+class Double(SimpleType):
+    name = 'xsd:double'
+
+    def xmlvalue(self, value):
+        return str(value)
+
+    def pythonvalue(self, value):
+        return _Decimal(value)
+
+
+class Duration(SimpleType):
+    name = 'xsd:duration'
+
+    def xmlvalue(self, value):
+        return value
 
     def pythonvalue(self, value):
         return value
@@ -93,34 +142,111 @@ class Time(SimpleType):
         return value
 
 
-class Double(SimpleType):
-    name = 'xsd:double'
+class Date(SimpleType):
+    name = 'xsd:date'
 
     def xmlvalue(self, value):
-        return str(value)
+        return value.strftime('%Y-%m-%d')
 
     def pythonvalue(self, value):
-        return float(value)
+        return value
 
 
-class Float(SimpleType):
-    name = 'xsd:float'
+class gYearMonth(SimpleType):
+    name = 'xsd:gYearMonth'
+
+
+class gYear(SimpleType):
+    name = 'xsd:gYear'
+
+
+class gMonthDay(SimpleType):
+    name = 'xsd:gMonthDay'
+
+
+class gDay(SimpleType):
+    name = 'xsd:gDay'
+
+
+class gMonth(SimpleType):
+    name = 'xsd:gMonth'
+
+
+class HexBinary(SimpleType):
+    name = 'xsd:hexBinary'
+
+
+class Base64Binary(SimpleType):
+    name = 'xsd:base64Binary'
+
+
+class AnyURI(SimpleType):
+    name = 'xsd:anyURI'
 
     def xmlvalue(self, value):
-        return str(value)
+        return value
 
     def pythonvalue(self, value):
-        return float(value)
+        return value
 
 
-class Decimal(SimpleType):
-    name = 'xsd:decimal'
+class QName(SimpleType):
+    pass
 
-    def xmlvalue(self, value):
-        return str(value)
 
-    def pythonvalue(self, value):
-        return _Decimal(value)
+class Notation(SimpleType):
+    pass
+
+
+##
+# Derived datatypes
+
+class NormalizedString(String):
+    name = 'xsd:normalizedString'
+
+
+class Token(NormalizedString):
+    name = 'xsd:token'
+
+
+class Language(Token):
+    name = 'xsd:language'
+
+
+class NmToken(Token):
+    name = 'xsd:NMTOKEN'
+
+
+class NmTokens(NmToken):
+    name = 'xsd:NMTOKENS'
+
+
+class Name(Token):
+    name = 'xsd:Name'
+
+
+class NCName(Name):
+    name = 'xsd:NCName'
+
+
+class ID(NCName):
+    name = 'xsd:ID'
+
+
+class IDREF(NCName):
+    name = 'xsd:IDREF'
+
+
+class IDREFS(IDREF):
+    name = 'xsd:IDREFS'
+
+
+class Entity(NCName):
+    name = 'xsd:ENTITY'
+
+
+class Entities(Entity):
+    name = 'xsd:ENTITIES'
 
 
 class Integer(Decimal):
@@ -133,6 +259,14 @@ class Integer(Decimal):
         return int(value)
 
 
+class NonPositiveInteger(Integer):
+    name = 'xsd:nonPositiveInteger'
+
+
+class NegativeInteger(Integer):
+    name = 'xsd:negativeInteger'
+
+
 class Long(Integer):
     name = 'xsd:long'
 
@@ -140,44 +274,106 @@ class Long(Integer):
         return long(value)
 
 
-class Short(Integer):
+class Int(Long):
+    name = 'xsd:int'
+
+
+class Short(Int):
     name = 'xsd:short'
 
 
-class UnsignedInt(Integer):
-    name = 'xsd:UnsignedInt'
+class Byte(Short):
+    """A signed 8-bit integer"""
+    name = 'xsd:byte'
 
 
-class UnsignedLong(Long):
-    name = 'xsd:UnsignedLong'
+class NonNegativeInteger(Integer):
+    name = 'xsd:nonNegativeInteger'
 
 
-class UnsignedShort(Short):
-    name = 'xsd:UnsignedShort'
+class UnsignedLong(NonNegativeInteger):
+    name = 'xsd:unsignedLong'
 
+
+class UnsignedInt(UnsignedLong):
+    name = 'xsd:unsignedInt'
+
+
+class UnsignedShort(UnsignedInt):
+    name = 'xsd:unsignedShort'
+
+
+class UnsignedByte(UnsignedShort):
+    name = 'xsd:unsignedByte'
+
+
+class PositiveInteger(NonNegativeInteger):
+    name = 'xsd:positiveInteger'
+
+
+##
+# Other
+
+class AnyType(SimpleType):
+    name = 'xsd:anyType'
+
+    def xmlvalue(self, value):
+        return value
+
+    def pythonvalue(self, value):
+        return value
 
 
 default_types = {
+
+    # Primitive
+    '{http://www.w3.org/2001/XMLSchema}string': String(),
+    '{http://www.w3.org/2001/XMLSchema}boolean': Boolean(),
+    '{http://www.w3.org/2001/XMLSchema}decimal': Decimal(),
+    '{http://www.w3.org/2001/XMLSchema}float': Float(),
+    '{http://www.w3.org/2001/XMLSchema}double': Double(),
+    '{http://www.w3.org/2001/XMLSchema}duration': Duration(),
+    '{http://www.w3.org/2001/XMLSchema}dateTime': DateTime(),
+    '{http://www.w3.org/2001/XMLSchema}time': Time(),
+    '{http://www.w3.org/2001/XMLSchema}date': Date(),
+    '{http://www.w3.org/2001/XMLSchema}gYearMonth': gYearMonth(),
+    '{http://www.w3.org/2001/XMLSchema}gYear': gYear(),
+    '{http://www.w3.org/2001/XMLSchema}gMonthDay': gMonthDay(),
+    '{http://www.w3.org/2001/XMLSchema}gDay': gDay(),
+    '{http://www.w3.org/2001/XMLSchema}gMonth': gMonth(),
+    '{http://www.w3.org/2001/XMLSchema}hexBinary': HexBinary(),
+    '{http://www.w3.org/2001/XMLSchema}base64Binary': Base64Binary(),
+    '{http://www.w3.org/2001/XMLSchema}anyURI': AnyURI(),
+    '{http://www.w3.org/2001/XMLSchema}QName': QName(),
+    '{http://www.w3.org/2001/XMLSchema}NOTATION': Notation(),
+
+    # Derived
+    '{http://www.w3.org/2001/XMLSchema}normalizedString': ID(),
+    '{http://www.w3.org/2001/XMLSchema}token': ID(),
+    '{http://www.w3.org/2001/XMLSchema}language': ID(),
+    '{http://www.w3.org/2001/XMLSchema}NMTOKEN': ID(),
+    '{http://www.w3.org/2001/XMLSchema}NMTOKENS': ID(),
+    '{http://www.w3.org/2001/XMLSchema}Name': ID(),
+    '{http://www.w3.org/2001/XMLSchema}NCName': ID(),
     '{http://www.w3.org/2001/XMLSchema}ID': ID(),
     '{http://www.w3.org/2001/XMLSchema}IDREF': IDREF(),
-    '{http://www.w3.org/2001/XMLSchema}QName': QName(),
-    '{http://www.w3.org/2001/XMLSchema}anyType': AnyType(),
-    '{http://www.w3.org/2001/XMLSchema}anyURI': AnyURI(),
-    '{http://www.w3.org/2001/XMLSchema}base64Binary': Base64Binary(),
-    '{http://www.w3.org/2001/XMLSchema}boolean': Boolean(),
-    '{http://www.w3.org/2001/XMLSchema}byte': Byte(),
-    '{http://www.w3.org/2001/XMLSchema}date': Date(),
-    '{http://www.w3.org/2001/XMLSchema}time': Time(),
-    '{http://www.w3.org/2001/XMLSchema}dateTime': DateTime(),
-    '{http://www.w3.org/2001/XMLSchema}decimal': Decimal(),
-    '{http://www.w3.org/2001/XMLSchema}double': Double(),
-    '{http://www.w3.org/2001/XMLSchema}float': Float(),
-    '{http://www.w3.org/2001/XMLSchema}int': Integer(),
+    '{http://www.w3.org/2001/XMLSchema}IDREFS': IDREFS(),
+    '{http://www.w3.org/2001/XMLSchema}ENTITY': Entity(),
+    '{http://www.w3.org/2001/XMLSchema}ENTITIES': Entities(),
+    '{http://www.w3.org/2001/XMLSchema}integer': Integer(),
+    '{http://www.w3.org/2001/XMLSchema}nonPositiveInteger': NonPositiveInteger(),  # noqa
+    '{http://www.w3.org/2001/XMLSchema}negativeInteger': NegativeInteger(),
     '{http://www.w3.org/2001/XMLSchema}long': Long(),
+    '{http://www.w3.org/2001/XMLSchema}int': Int(),
     '{http://www.w3.org/2001/XMLSchema}short': Short(),
-    '{http://www.w3.org/2001/XMLSchema}string': String(),
+    '{http://www.w3.org/2001/XMLSchema}byte': Byte(),
+    '{http://www.w3.org/2001/XMLSchema}nonNegativeInteger': NonNegativeInteger(),  # noqa
     '{http://www.w3.org/2001/XMLSchema}unsignedByte': String(),
     '{http://www.w3.org/2001/XMLSchema}unsignedInt': UnsignedInt(),
     '{http://www.w3.org/2001/XMLSchema}unsignedLong': UnsignedLong(),
     '{http://www.w3.org/2001/XMLSchema}unsignedShort': UnsignedShort(),
+    '{http://www.w3.org/2001/XMLSchema}positiveInteger': PositiveInteger(),
+
+    # Other
+    '{http://www.w3.org/2001/XMLSchema}anyType': AnyType(),
 }
---
+++ tests/test_xsd_builtins.py
@@ -0,0 +1,72 @@
+from decimal import Decimal as D
+from zeep.xsd import builtins
+
+
+class TestString:
+
+    def test_xmlvalue(self):
+        instance = builtins.String()
+        result = instance.xmlvalue('foobar')
+        assert result == 'foobar'
+
+    def test_pythonvalue(self):
+        instance = builtins.String()
+        result = instance.pythonvalue('foobar')
+        assert result == 'foobar'
+
+
+class TestBoolean:
+
+    def test_xmlvalue(self):
+        instance = builtins.Boolean()
+        assert instance.xmlvalue(True) == 'true'
+        assert instance.xmlvalue(False) == 'false'
+        assert instance.xmlvalue(1) == 'true'
+        assert instance.xmlvalue(0) == 'false'
+
+    def test_pythonvalue(self):
+        instance = builtins.Boolean()
+        assert instance.pythonvalue('1') is True
+        assert instance.pythonvalue('true') is True
+        assert instance.pythonvalue('0') is False
+        assert instance.pythonvalue('false') is False
+
+
+class TestDecimal:
+
+    def test_xmlvalue(self):
+        instance = builtins.Decimal()
+        assert instance.xmlvalue(D('10.00')) == '10.00'
+        assert instance.xmlvalue(D('10.000002')) == '10.000002'
+        assert instance.xmlvalue(D('10.000002')) == '10.000002'
+        assert instance.xmlvalue(D('10')) == '10'
+        assert instance.xmlvalue(D('-10')) == '-10'
+
+    def test_pythonvalue(self):
+        instance = builtins.Decimal()
+        assert instance.pythonvalue('10') == D('10')
+        assert instance.pythonvalue('10.001') == D('10.001')
+        assert instance.pythonvalue('+10.001') == D('10.001')
+        assert instance.pythonvalue('-10.001') == D('-10.001')
+
+
+class TestFloat:
+
+    def test_xmlvalue(self):
+        instance = builtins.Float()
+        assert instance.xmlvalue(float(10)) == '10.0'
+        assert instance.xmlvalue(float(3.9999)) == '3.9999'
+        assert instance.xmlvalue(float('inf')) == 'INF'
+        assert instance.xmlvalue(float('1267.43233E12')) == '1.26743233E+15'
+        assert instance.xmlvalue(float(12.78e-2)) == '0.1278'
+
+    def test_pythonvalue(self):
+        instance = builtins.Float()
+        assert instance.pythonvalue('10') == float('10')
+        assert instance.pythonvalue('-1E4') == float('-1E4')
+        assert instance.pythonvalue('1267.43233E12') == float('1267.43233E12')
+        assert instance.pythonvalue('12.78e-2') == float('0.1278')
+        assert instance.pythonvalue('12') == float(12)
+        assert instance.pythonvalue('-0') == float(0)
+        assert instance.pythonvalue('0') == float(0)
+        assert instance.pythonvalue('INF') == float('inf')
