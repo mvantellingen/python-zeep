b426fa975f92bfd1195db417011056b9d68686a1
Michael van Tellingen <michael@mvantellingen.nl>
2016-04-05 22:19:25 +0200

    Update to beta status.. :P

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
 
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
