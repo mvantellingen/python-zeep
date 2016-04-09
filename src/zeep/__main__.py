4466eeeeda0eb9f82abefd2f530cca097a6e279b
Michael van Tellingen <michael@mvantellingen.nl>
2016-04-09 16:02:59 +0200

    Use argparse instead of hacky old solution

--- src/zeep/__main__.py
+++ src/zeep/__main__.py
@@ -1,15 +1,31 @@
 from __future__ import absolute_import, print_function
 
+import argparse
 import logging
 import logging.config
-import time
 import sys
+import time
+
+from zeep.cache import SqliteCache
+from zeep.client import Client
 
 logger = logging.getLogger('zeep')
 
+
 if __name__ == '__main__':
-    use_persistent_cache = '--cache' in sys.argv
-    if '--verbose' in sys.argv:
+    parser = argparse.ArgumentParser(description='Zeep: The SOAP client')
+    parser.add_argument(
+        'wsdl_file', type=str, help='Path or URL to the WSDL file')
+    parser.add_argument(
+        '--cache', action='store_true', help='Enable cache')
+    parser.add_argument(
+        '--verbose', action='store_true', help='Enable verbose output')
+    parser.add_argument(
+        '--profile', help="Enable profiling and save output to given file")
+
+    args = parser.parse_args()
+
+    if args.verbose:
         logging.config.dictConfig({
             'version': 1,
             'formatters': {
@@ -38,15 +54,18 @@ if __name__ == '__main__':
             }
         })
 
-    from zeep.client import Client
-    from zeep.cache import SqliteCache
+    if args.profile:
+        import cProfile
+        profile = cProfile.Profile()
+        profile.enable()
 
-    if len(sys.argv) < 2:
-        print("Missing argument. Please pass a WSDL", file=sys.stderr)
-        sys.exit(1)
-
-    cache = SqliteCache(persistent=use_persistent_cache)
+    cache = SqliteCache(persistent=args.cache)
     st = time.time()
     client = Client(sys.argv[1], cache=cache)
     logger.debug("Loading WSDL took %sms", (time.time() - st) * 1000)
-    sys.exit(0)
+
+    if args.profile:
+        profile.disable()
+        profile.dump_stats(args.profile)
+
+    print(client.wsdl.dump())
