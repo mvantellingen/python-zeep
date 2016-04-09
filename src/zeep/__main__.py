dd3bfc54c70c47e9dca9d240cc82c3bc6c67296d
Michael van Tellingen <michael@mvantellingen.nl>
2016-04-09 15:27:09 +0200

    Improve logging  config for __main__

--- src/zeep/__main__.py
+++ src/zeep/__main__.py
@@ -1,12 +1,42 @@
 from __future__ import absolute_import, print_function
 
 import logging
+import logging.config
+import time
 import sys
+
+logger = logging.getLogger('zeep')
 
 if __name__ == '__main__':
     use_persistent_cache = '--cache' in sys.argv
     if '--verbose' in sys.argv:
-        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+        logging.config.dictConfig({
+            'version': 1,
+            'formatters': {
+                'verbose': {
+                    'format': '%(name)s: %(message)s'
+                }
+            },
+            'handlers': {
+                'console': {
+                    'level': 'DEBUG',
+                    'class': 'logging.StreamHandler',
+                    'formatter': 'verbose',
+                },
+            },
+            'loggers': {
+                'zeep': {
+                    'level': 'DEBUG',
+                    'propagate': True,
+                    'handlers': ['console'],
+                },
+                'zeep.xsd': {
+                    'level': 'INFO',
+                    'propagate': True,
+                    'handlers': ['console'],
+                },
+            }
+        })
 
     from zeep.client import Client
     from zeep.cache import SqliteCache
@@ -16,6 +46,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     cache = SqliteCache(persistent=use_persistent_cache)
+    st = time.time()
     client = Client(sys.argv[1], cache=cache)
-    client.wsdl.dump()
+    logger.debug("Loading WSDL took %sms", (time.time() - st) * 1000)
     sys.exit(0)
