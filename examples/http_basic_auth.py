d746a6aee55e0c8903822966d7d3de0b76a61c80
mcordes <cordes.matthew@gmail.com>
2016-05-23 15:47:50 -0400

    Support for HTTP authentication (#32)
    * support HTTP basic authentication
    
    * pep8

---
+++ examples/http_basic_auth.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import zeep
+from zeep.transports import Transport
+
+# Example using basic authentication with a webservice 
+
+transport_with_basic_auth = Transport(http_auth=('username', 'password'))
+
+client = zeep.Client(
+    wsdl='http://nonexistent?WSDL'
+    transport=transport_with_basic_auth
+)
+
+client.wsdl.dump()
--- src/zeep/transports.py
+++ src/zeep/transports.py
@@ -5,10 +5,11 @@ from zeep.cache import SqliteCache
 
 class Transport(object):
 
-    def __init__(self, cache=None, timeout=300, verify=True):
+    def __init__(self, cache=None, timeout=300, verify=True, http_auth=None):
         self.cache = cache or SqliteCache()
         self.timeout = timeout
         self.verify = verify
+        self.http_auth = http_auth
 
     def load(self, url):
         if self.cache:
@@ -16,7 +17,8 @@ class Transport(object):
             if response:
                 return bytes(response)
 
-        response = requests.get(url, timeout=self.timeout, verify=self.verify)
+        response = requests.get(url, timeout=self.timeout, verify=self.verify,
+                                auth=self.http_auth)
 
         if self.cache:
             self.cache.add(url, response.content)
@@ -25,12 +27,14 @@ class Transport(object):
 
     def post(self, address, message, headers):
         response = requests.post(
-            address, data=message, headers=headers, verify=self.verify
+            address, data=message, headers=headers, verify=self.verify,
+            auth=self.http_auth
         )
         return response
 
     def get(self, address, params, headers):
         response = requests.get(
-            address, params=params, headers=headers, verify=self.verify
+            address, params=params, headers=headers, verify=self.verify,
+            auth=self.http_auth
         )
         return response
