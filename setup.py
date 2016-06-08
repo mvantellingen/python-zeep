6ecd1483a12d1f1940e8f36227ec33d802dd1729
Michael van Tellingen <michael@mvantellingen.nl>
2016-06-08 20:35:24 +0200

    Prepare for 0.8.1 release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ tests_require = [
 
 setup(
     name='zeep',
-    version='0.9.0.dev0',
+    version='0.8.1',
     description='A modern/fast Python SOAP client based on lxml / requests',
     long_description=open('README.rst').read(),
     author="Michael van Tellingen",
