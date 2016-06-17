c455764319d00e1e0984bb742157943ead3c59ad
Michael van Tellingen <michael@mvantellingen.nl>
2016-06-17 20:59:34 +0200

    Release 0.9.1 with soapaction header fixes

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ tests_require = [
 
 setup(
     name='zeep',
-    version='0.10.0.dev0',
+    version='0.9.1',
     description='A modern/fast Python SOAP client based on lxml / requests',
     long_description=open('README.rst').read(),
     author="Michael van Tellingen",
