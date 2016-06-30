6041a58461f77b6c06f817ced9d02671b03f979f
Michael van Tellingen <michael@mvantellingen.nl>
2016-06-30 19:30:28 +0200

    Properly handle the reserved xml namespace
    Docs:
    
    The prefix xml is by definition bound to the namespace name
    http://www.w3.org/XML/1998/namespace. It MAY, but need not, be declared,
    and MUST NOT be bound to any other namespace name. Other prefixes
    MUST NOT be bound to this namespace name, and it MUST NOT be declared
    as the default namespace.

--- src/zeep/xsd/schema.py
+++ src/zeep/xsd/schema.py
@@ -254,7 +254,14 @@ class SchemaDocument(object):
             ) % (self._target_namespace, ns))
 
     def _create_qname(self, name):
-        return etree.QName(name) if not isinstance(name, etree.QName) else name
+        if not isinstance(name, etree.QName):
+            name = etree.QName(name)
+
+        # Handle reserved namespace
+        if name.namespace == 'xml':
+            name = etree.QName(
+                'http://www.w3.org/XML/1998/namespace', name.localname)
+        return name
 
     @property
     def is_empty(self):
