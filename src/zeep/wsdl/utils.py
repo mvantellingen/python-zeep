e16e7f386d1e913a72c691aafa638008f077f79a
vashek <vashek+github@gmail.com>
2016-09-21 00:50:25 +0200

    fix Header namespace for both SOAP 1.1 and 1.2

--- src/zeep/wsdl/utils.py
+++ src/zeep/wsdl/utils.py
@@ -2,7 +2,14 @@ from lxml import etree
 
 
 def get_or_create_header(envelope):
-    header_qname = '{http://www.w3.org/2003/05/soap-envelope}Header'
+    # late import to avoid cyclic imports
+    from zeep.wsdl.soap import Soap11Binding, Soap12Binding
+    # find the namespace of the SOAP Envelope (because it's different for SOAP 1.1 and 1.2)
+    root_tag = etree.QName(envelope)
+    soap_envelope_namespace = root_tag.namespace
+    assert soap_envelope_namespace in (Soap11Binding.nsmap['soap-env'], Soap12Binding.nsmap['soap-env'])
+    # look for the Header element and create it if not found
+    header_qname = '{%s}Header' % soap_envelope_namespace
     header = envelope.find(header_qname)
     if header is None:
         header = etree.Element(header_qname)
