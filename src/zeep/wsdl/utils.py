fe4068f73501be11dcd148a1a2578a92213e4ad6
vashek <vashek+github@gmail.com>
2016-09-21 00:23:22 +0200

    fix SOAP Header namespace (see issue 92)

--- src/zeep/wsdl/utils.py
+++ src/zeep/wsdl/utils.py
@@ -2,7 +2,7 @@ from lxml import etree
 
 
 def get_or_create_header(envelope):
-    header_qname = '{http://schemas.xmlsoap.org/soap/envelope/}Header'
+    header_qname = '{http://www.w3.org/2003/05/soap-envelope}Header'
     header = envelope.find(header_qname)
     if header is None:
         header = etree.Element(header_qname)
