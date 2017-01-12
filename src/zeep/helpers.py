1f4842eb8d4b46a57491aef962bf59bbbe886737
Michael Palumbo <michael.palumbo87@gmail.com>
2017-01-12 13:21:26 -0800

    Allow to change the dict type used.

--- src/zeep/helpers.py
+++ src/zeep/helpers.py
@@ -5,7 +5,7 @@ from lxml import etree
 from zeep.xsd.valueobjects import CompoundValue
 
 
-def serialize_object(obj):
+def serialize_object(obj, dict_type=OrderedDict):
     """Serialize zeep objects to native python data structures"""
     if obj is None:
         return obj
@@ -17,7 +17,7 @@ def serialize_object(obj):
         return [serialize_object(sub) for sub in obj]
 
     if isinstance(obj, dict):
-        result = OrderedDict()
+        result = dict_type()
         for key in obj:
             value = obj[key]
             if isinstance(value, (list, CompoundValue)):
