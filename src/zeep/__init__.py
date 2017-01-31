73c566ab68898f66cab0ad18ffbd9c0a297346df
Michael van Tellingen <michael@mvantellingen.nl>
2017-01-31 22:02:43 +0100

    Bump version: 0.27.0 → 1.0.0

--- src/zeep/__init__.py
+++ src/zeep/__init__.py
@@ -3,4 +3,4 @@ from zeep.transports import Transport  # noqa
 from zeep.plugins import Plugin  # noqa
 from zeep.xsd.valueobjects import AnyObject  # noqa
 
-__version__ = '0.27.0'
+__version__ = '1.0.0'
