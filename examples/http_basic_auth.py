7b1b47af6f07bdb321f8fca32c1b7e8bdfd154dc
Michael van Tellingen <michael@mvantellingen.nl>
2017-05-25 08:01:09 +0200

    Update the http-basic-auth example script

--- examples/http_basic_auth.py
+++ examples/http_basic_auth.py
@@ -1,10 +1,16 @@
 from __future__ import print_function
+
+from requests import Session
+from requests.auth import HTTPBasicAuth
+
 import zeep
 from zeep.transports import Transport
 
 # Example using basic authentication with a webservice
 
-transport_with_basic_auth = Transport(http_auth=('username', 'password'))
+session = Session()
+session.auth = HTTPBasicAuth('username', 'password')
+transport_with_basic_auth = Transport(session=session)
 
 client = zeep.Client(
     wsdl='http://nonexistent?WSDL',
