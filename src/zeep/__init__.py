7ba45e95ab5c9667b3c73c4e37d1b3a226ae2aa4
Bruno Thalmann <mr.thalmann@gmail.com>
2018-06-21 18:13:10 +0200

    Import content interceptor

--- src/zeep/__init__.py
+++ src/zeep/__init__.py
@@ -1,6 +1,6 @@
 from zeep.client import CachingClient, Client  # noqa
 from zeep.transports import Transport  # noqa
-from zeep.plugins import Plugin  # noqa
+from zeep.plugins import Plugin, ContentInterceptor  # noqa
 from zeep.xsd.valueobjects import AnyObject  # noqa
 
 __version__ = '2.4.0'
