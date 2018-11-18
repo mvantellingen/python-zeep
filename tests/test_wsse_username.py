720156a63d3f78c3fafb8ea59a2fa41d0c3e062c
Vishu <iamno.16@gmail.com>
2018-11-18 10:05:00 -0600

    - Adding an unit test for timestamp token in WSSE header.

--- tests/test_wsse_username.py
+++ tests/test_wsse_username.py
@@ -1,13 +1,13 @@
 import datetime
 import os
-
 import pytest
 import requests_mock
-from freezegun import freeze_time
 
+from freezegun import freeze_time
 from tests.utils import assert_nodes_equal, load_xml
 from zeep import client
 from zeep.wsse import UsernameToken
+from zeep.wsse.utils import WSU
 
 
 @pytest.mark.requests
@@ -235,4 +235,68 @@ def test_password_prepared():
           </soap-env:Body>
         </soap-env:Envelope>
     """  # noqa
+    assert_nodes_equal(envelope, expected)
+
+
+def test_timestamp_token():
+    envelope = load_xml("""
+            <soap-env:Envelope
+                xmlns:ns0="http://example.com/stockquote.xsd"
+                xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/"
+                xmlns:soap-env="http://schemas.xmlsoap.org/soap/envelope/"
+                xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/"
+                xmlns:wsu="http://schemas.xmlsoap.org/ws/2003/06/utility"
+                xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+            >
+              <soap-env:Header xmlns:ns0="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+                <ns0:Security>
+                  <ns0:UsernameToken/>
+                </ns0:Security>
+              </soap-env:Header>
+              <soap-env:Body>
+                <ns0:TradePriceRequest>
+                  <tickerSymbol>foobar</tickerSymbol>
+                  <ns0:country/>
+                </ns0:TradePriceRequest>
+              </soap-env:Body>
+            </soap-env:Envelope>
+        """)  # noqa
+
+    timestamp_token = WSU.Timestamp()
+    timestamp_token.attrib['Id'] = 'id-21a27a50-9ebf-49cc-96bf-fcf7131e7858'
+    some_date_obj = datetime.datetime(2018, 11, 18, 15, 44, 27, 440252)
+    timestamp_elements = [WSU.Created(some_date_obj.strftime("%Y-%m-%dT%H:%M:%SZ")),
+                          WSU.Expires((some_date_obj + datetime.timedelta(minutes=10)).strftime("%Y-%m-%dT%H:%M:%SZ"))]
+    timestamp_token.extend(timestamp_elements)
+
+    token = UsernameToken('Vishu', 'Guntupalli', timestamp_token=timestamp_token)
+    envelope, headers = token.apply(envelope, {})
+    expected = """
+            <soap-env:Envelope
+                xmlns:ns0="http://example.com/stockquote.xsd"
+                xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/"
+                xmlns:soap-env="http://schemas.xmlsoap.org/soap/envelope/"
+                xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/"
+                xmlns:wsu="http://schemas.xmlsoap.org/ws/2003/06/utility"
+                xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+              <soap-env:Header xmlns:ns0="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+                <ns0:Security>
+                  <ns0:UsernameToken>
+                    <ns0:Username>Vishu</ns0:Username>
+                    <ns0:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">Guntupalli</ns0:Password>
+                  </ns0:UsernameToken>
+                  <wsu:Timestamp xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" Id="id-21a27a50-9ebf-49cc-96bf-fcf7131e7858">
+                       <wsu:Created>2018-11-18T15:44:27Z</wsu:Created>
+                       <wsu:Expires>2018-11-18T15:54:27Z</wsu:Expires>
+                  </wsu:Timestamp>
+                </ns0:Security>
+              </soap-env:Header>
+              <soap-env:Body>
+                <ns0:TradePriceRequest>
+                  <tickerSymbol>foobar</tickerSymbol>
+                  <ns0:country/>
+                </ns0:TradePriceRequest>
+              </soap-env:Body>
+            </soap-env:Envelope>
+        """  # noqa
     assert_nodes_equal(envelope, expected)
