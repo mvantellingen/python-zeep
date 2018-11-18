20c7990620f39c2b05958727d21f3216ea3a71ff
Michael van Tellingen <michael@mvantellingen.nl>
2018-11-18 11:57:48 +0100

    Run the tornado transport load outside event loop during test

--- tests/test_tornado_transport.py
+++ tests/test_tornado_transport.py
@@ -9,27 +9,27 @@ from tornado.testing import AsyncTestCase, gen_test
 from zeep.tornado import TornadoAsyncTransport
 
 
+@pytest.mark.requests
+@patch('tornado.httpclient.HTTPClient.fetch')
+def test_tornado_load(mock_httpclient_fetch):
+    cache = stub(get=lambda url: None, add=lambda url, content: None)
+    response = HTTPResponse(HTTPRequest('http://tests.python-zeep.org/test.xml'), 200)
+    response.buffer = True
+    response._body = 'x'
+    mock_httpclient_fetch.return_value = response
+
+    transport = TornadoAsyncTransport(cache=cache)
+
+    result = transport.load('http://tests.python-zeep.org/test.xml')
+
+    assert result == 'x'
+
+
 class TornadoAsyncTransportTest(AsyncTestCase):
     @pytest.mark.requests
     def test_no_cache(self):
         transport = TornadoAsyncTransport()
         assert transport.cache is None
-
-    @pytest.mark.requests
-    @patch('tornado.httpclient.HTTPClient.fetch')
-    @gen_test
-    def test_load(self, mock_httpclient_fetch):
-        cache = stub(get=lambda url: None, add=lambda url, content: None)
-        response = HTTPResponse(HTTPRequest('http://tests.python-zeep.org/test.xml'), 200)
-        response.buffer = True
-        response._body = 'x'
-        mock_httpclient_fetch.return_value = response
-
-        transport = TornadoAsyncTransport(cache=cache)
-
-        result = transport.load('http://tests.python-zeep.org/test.xml')
-
-        assert result == 'x'
 
     @pytest.mark.requests
     @patch('tornado.httpclient.AsyncHTTPClient.fetch')
