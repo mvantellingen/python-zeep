65ef6f3f14a4619ecaa710b9b6551286fa32bbe1
Michael van Tellingen <michael@mvantellingen.nl>
2018-12-17 09:59:55 +0100

    Bump version: 3.1.0 → 3.2.0

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ with open('README.rst') as fh:
 
 setup(
     name='zeep',
-    version='3.1.0',
+    version='3.2.0',
     description='A modern/fast Python SOAP client based on lxml / requests',
     long_description=long_description,
     author="Michael van Tellingen",
