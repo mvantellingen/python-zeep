ba62736f43074ee32b2127c497d8743efb01e114
StevenXue <15994039246@163.com>
2019-05-22 15:55:40 +0800

    other fix

--- src/zeep/xsd/types/complex.py
+++ src/zeep/xsd/types/complex.py
@@ -242,17 +242,17 @@ class ComplexType(AnyType):
         if value is None:
             value = {}
 
-        if isinstance(value, str) or isinstance(value, unicode):
-            # Solve the value of a long string containing certain fields, such as id, href, _attr_1, etc.
-            # unexpected string value, attr_value set to NotSet by default
-            value = {}
-
         if isinstance(value, ArrayValue):
             value = value.as_value_object()
 
         # Render attributes
         for name, attribute in self.attributes:
-            attr_value = value[name] if name in value else NotSet
+            if isinstance(value, str) or isinstance(value, unicode):
+                # Solve the value of a long string containing certain fields, such as id, href, _attr_1, etc.
+                # unexpected string value, attr_value set to NotSet by default
+                attr_value = NotSet
+            else:
+                attr_value = value[name] if name in value else NotSet
             child_path = render_path + [name]
             attribute.render(parent, attr_value, child_path)
 
