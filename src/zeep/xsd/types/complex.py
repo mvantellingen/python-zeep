0e8707da69d3a178a458190727954bd8af30f1bd
StevenXue <15994039246@163.com>
2019-05-27 16:13:51 +0800

    python version judge

--- src/zeep/xsd/types/complex.py
+++ src/zeep/xsd/types/complex.py
@@ -247,12 +247,17 @@ class ComplexType(AnyType):
 
         # Render attributes
         for name, attribute in self.attributes:
-            if isinstance(value, str) or isinstance(value, unicode):
-                # Solve the value of a long string containing certain fields, such as id, href, _attr_1, etc.
-                # unexpected string value, attr_value set to NotSet by default
-                attr_value = NotSet
+            import sys
+            if sys.version_info < (3, 4):
+                if isinstance(value, str) or isinstance(value, unicode):
+                    attr_value = NotSet
+                else:
+                    attr_value = value[name] if name in value else NotSet
             else:
-                attr_value = value[name] if name in value else NotSet
+                if isinstance(value, str):
+                    attr_value = NotSet
+                else:
+                    attr_value = value[name] if name in value else NotSet
             child_path = render_path + [name]
             attribute.render(parent, attr_value, child_path)
 
