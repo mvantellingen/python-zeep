d67bd9df74026de4b326fd6002678cb8b4ab26b4
nofalx <nofal.ahmad@outlook.com>
2019-11-05 15:15:47 +0400

    added test case

--- tests/test_xsd_builtins.py
+++ tests/test_xsd_builtins.py
@@ -135,6 +135,9 @@ class TestDateTime:
         value = datetime.datetime(2016, 3, 4, 21, 14, 42)
         assert instance.pythonvalue("2016-03-04T21:14:42") == value
 
+        value = datetime.datetime(2016, 3, 4, 0, 0, 0)
+        assert instance.pythonvalue("2016-03-04 00:00:00") == value
+
         value = datetime.datetime(2016, 3, 4, 21, 14, 42, 123456)
         assert instance.pythonvalue("2016-03-04T21:14:42.123456") == value
 
