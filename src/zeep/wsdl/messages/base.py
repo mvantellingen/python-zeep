mvantellingen/python-zeep

commit 965a5acf357c08716db079feb63d9a52a1857430

Refactor the signature generation.
Never render nested global elements or types instead render
their prefixed name instead. This allows the removal of the depth
parameter. To get the proper prefixed name it is also now possible
to pass the schema to the signature() method so that the custom
prefix mapping is used.

The builtin xsd elements and types are now also handled as a
regular xsd:schema which allows some cleaning up

src/zeep/wsdl/messages/base.py
@@ -31,15 +31,17 @@ class ConcreteMessage(object):
             if isinstance(self.body.type, xsd.ComplexType):
                 try:
                     if len(self.body.type.elements) == 1:
-                        return self.body.type.elements[0][1].type.signature()
+                        return self.body.type.elements[0][1].type.signature(
+                            schema=self.wsdl.types, standalone=False)
                 except AttributeError:
                     return None
 
-            return self.body.type.signature()
+            return self.body.type.signature(schema=self.wsdl.types, standalone=False)
 
-        parts = [self.body.type.signature()]
+        parts = [self.body.type.signature(schema=self.wsdl.types, standalone=False)]
         if getattr(self, 'header', None):
-            parts.append('_soapheaders={%s}' % self.header.signature())
+            parts.append('_soapheaders={%s}' % self.header.signature(
+                schema=self.wsdl.types), standalone=False)
         return ', '.join(part for part in parts if part)
 
     @classmethod

src/zeep/xsd/elements/base.py
@@ -28,5 +28,5 @@ class Base(object):
         """Consume matching xmlelements and call parse() on each of them"""
         raise NotImplementedError()
 
-    def signature(self, depth=()):
+    def signature(self, schema=None):
         return ''

src/zeep/xsd/types/base.py
@@ -1,3 +1,6 @@
+from zeep.xsd.utils import create_prefixed_name
+
+
 class Type(object):
 
     def __init__(self, qname=None, is_global=False):
@@ -5,6 +8,9 @@ class Type(object):
         self.name = qname.localname if qname else None
         self._resolved = False
         self.is_global = is_global
+
+    def get_prefixed_name(self, schema):
+        return create_prefixed_name(self.qname, schema)
 
     def accept(self, value):
         raise NotImplementedError
@@ -51,7 +57,7 @@ class Type(object):
         return []
 
     @classmethod
-    def signature(cls, depth=()):
+    def signature(cls, schema=None, standalone=True):
         return ''
 
 

src/zeep/ns.py
@@ -16,3 +16,7 @@ WSA = 'http://www.w3.org/2005/08/addressing'
 DS = 'http://www.w3.org/2000/09/xmldsig#'
 WSSE = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd'
 WSU = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd'
+
+NAMESPACE_TO_PREFIX = {
+    XSD: 'xsd',
+}

src/zeep/wsdl/messages/soap.py
@@ -110,14 +110,16 @@ class SoapMessage(ConcreteMessage):
             if isinstance(self.envelope.type, xsd.ComplexType):
                 try:
                     if len(self.envelope.type.elements) == 1:
-                        return self.envelope.type.elements[0][1].type.signature()
+                        return self.envelope.type.elements[0][1].type.signature(
+                            schema=self.wsdl.types, standalone=False)
                 except AttributeError:
                     return None
-            return self.envelope.type.signature()
+            return self.envelope.type.signature(schema=self.wsdl.types, standalone=False)
 
-        parts = [self.body.type.signature()]
+        parts = [self.body.type.signature(schema=self.wsdl.types, standalone=False)]
         if self.header.type._element:
-            parts.append('_soapheaders={%s}' % self.header.signature())
+            parts.append('_soapheaders={%s}' % self.header.type.signature(
+                schema=self.wsdl.types, standalone=False))
         return ', '.join(part for part in parts if part)
 
     @classmethod
@@ -272,15 +274,16 @@ class SoapMessage(ConcreteMessage):
         elements from the body and the headers.
 
         """
-        all_elements = xsd.Sequence([
-            xsd.Element('body', self.body.type),
-        ])
+        all_elements = xsd.Sequence([])
 
         if self.header.type._element:
             all_elements.append(
-                xsd.Element('header', self.header.type))
+                xsd.Element('{%s}header' % self.nsmap['soap-env'], self.header.type))
 
-        return xsd.Element('envelope', xsd.ComplexType(all_elements))
+        all_elements.append(
+            xsd.Element('{%s}body' % self.nsmap['soap-env'], self.body.type))
+
+        return xsd.Element('{%s}envelope' % self.nsmap['soap-env'], xsd.ComplexType(all_elements))
 
     def _serialize_header(self, headers_value, nsmap):
         if not headers_value:

src/zeep/wsdl/wsdl.py
@@ -75,8 +75,6 @@ class Document(object):
         return '<WSDL(location=%r)>' % self.location
 
     def dump(self):
-        namespaces = {v: k for k, v in self.types.prefix_map.items()}
-
         print('')
         print("Prefixes:")
         for prefix, namespace in self.types.prefix_map.items():
@@ -84,18 +82,14 @@ class Document(object):
 
         print('')
         print("Global elements:")
-        for elm_obj in sorted(self.types.elements, key=lambda k: six.text_type(k)):
-            value = six.text_type(elm_obj)
-            if hasattr(elm_obj, 'qname') and elm_obj.qname.namespace:
-                value = '%s:%s' % (namespaces[elm_obj.qname.namespace], value)
+        for elm_obj in sorted(self.types.elements, key=lambda k: k.qname):
+            value = elm_obj.signature(schema=self.types)
             print(' ' * 4, value)
 
         print('')
         print("Global types:")
         for type_obj in sorted(self.types.types, key=lambda k: k.qname or ''):
-            value = six.text_type(type_obj)
-            if getattr(type_obj, 'qname', None) and type_obj.qname.namespace:
-                value = '%s:%s' % (namespaces[type_obj.qname.namespace], value)
+            value = type_obj.signature(schema=self.types)
             print(' ' * 4, value)
 
         print('')

src/zeep/xsd/elements/any.py
@@ -188,7 +188,7 @@ class Any(Base):
     def resolve(self):
         return self
 
-    def signature(self, depth=()):
+    def signature(self, schema=None, standalone=True):
         if self.restrict:
             base = self.restrict.name
         else:
@@ -219,5 +219,5 @@ class AnyAttribute(Base):
         for name, val in value.items():
             parent.set(name, val)
 
-    def signature(self, depth=()):
+    def signature(self, schema=None, standalone=True):
         return '{}'

src/zeep/xsd/elements/attribute.py
@@ -88,5 +88,5 @@ class AttributeGroup(object):
         self._attributes = resolved
         return self
 
-    def signature(self, depth=()):
-        return ', '.join(attr.signature() for attr in self._attributes)
+    def signature(self, schema=None, standalone=True):
+        return ', '.join(attr.signature(schema) for attr in self._attributes)

commit be71670dc9e8e1549e385648bab8fc266cb8585a

Fixed signature generation for ConcreteMessage
This is broken since 965a5acf357c08716db079feb63d9a52a1857430

src/zeep/wsdl/messages/base.py
@@ -45,10 +45,13 @@ class ConcreteMessage:
             return self.body.type.signature(schema=self.wsdl.types, standalone=False)
 
         parts = [self.body.type.signature(schema=self.wsdl.types, standalone=False)]
+
+        # TODO: There was a bug in this part for a while, so wondering if this
+        # code is used
         if getattr(self, "header", None):
             parts.append(
-                "_soapheaders={%s}" % self.header.signature(schema=self.wsdl.types),
-                standalone=False,
+                "_soapheaders={%s}"
+                % self.header.signature(schema=self.wsdl.types, standalone=False)
             )
         return ", ".join(part for part in parts if part)
 