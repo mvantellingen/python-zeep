c56664a47c167934a5b88c9eee4e93c1d11a3607
Doug Addy <da1910@imperial.ac.uk>
2020-09-01 15:02:04 +0100

    Expose MemorySignature in WSSE module for import
    Closes #1127

--- src/zeep/wsse/__init__.py
+++ src/zeep/wsse/__init__.py
@@ -1,3 +1,3 @@
 from .compose import Compose  # noqa
-from .signature import BinarySignature, Signature  # noqa
+from .signature import BinarySignature, Signature, MemorySignature  # noqa
 from .username import UsernameToken  # noqa
