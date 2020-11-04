b82fd24018b14aa04ddab9bba4730a31044d0294
Pol <psanlorenzo@vinissimus.com>
2020-11-04 13:57:12 +0100

    fix

--- tests/test_async_client.py
+++ tests/test_async_client.py
@@ -6,5 +6,5 @@ from zeep import AsyncClient
 @pytest.mark.requests
 @pytest.mark.asyncio
 async def test_context_manager():
-    async with AsyncClient("tests/wsdl_files/soap.wsdl") as cli:
-        assert cli
+    async with AsyncClient("tests/wsdl_files/soap.wsdl") as async_client:
+        assert async_client
