c80519ef01216f8e5bcfe8de7995d841e03b0f2a
Michael van Tellingen <m.vantellingen@labdigital.nl>
2022-11-03 08:19:41 +0100

    Re-export AsyncTransport

--- src/zeep/__init__.py
+++ src/zeep/__init__.py
@@ -1,12 +1,13 @@
 from zeep.client import AsyncClient, CachingClient, Client
 from zeep.plugins import Plugin
 from zeep.settings import Settings
-from zeep.transports import Transport
+from zeep.transports import AsyncTransport, Transport
 from zeep.xsd.valueobjects import AnyObject
 
 __version__ = "4.1.0"
 __all__ = [
     "AsyncClient",
+    "AsyncTransport",
     "CachingClient",
     "Client",
     "Plugin",
