mvantellingen/python-zeep

commit 0c8a3fc4f9a88310ecaef0b1451cb6f870e38517

Add support to pass base-types to type extensions
The base type is now propageted through the xsd:extension for new
types so that for example soapenc:string still accepts str types since
it is an extension of xsd:string.

See #416

src/zeep/xsd/types/complex.py
@@ -33,6 +33,7 @@ class ComplexType(AnyType):
         self._attributes = attributes or []
         self._restriction = restriction
         self._extension = extension
+        self._extension_types = tuple()
         super(ComplexType, self).__init__(qname=qname, is_global=is_global)
 
     def __call__(self, *args, **kwargs):
@@ -40,7 +41,7 @@ class ComplexType(AnyType):
 
     @property
     def accepted_types(self):
-        return (self._value_class,)
+        return (self._value_class,) + self._extension_types
 
     @threaded_cached_property
     def _value_class(self):
@@ -190,6 +191,15 @@ class ComplexType(AnyType):
             child_path = render_path + [name]
             attribute.render(parent, attr_value, child_path)
 
+        if (
+            len(self.elements_nested) == 1
+            and isinstance(value, self.accepted_types)
+            and not isinstance(value, (list, dict, CompoundValue))
+        ):
+            element = self.elements_nested[0][1]
+            element.type.render(parent, value, None, child_path)
+            return
+
         # Render sub elements
         for name, element in self.elements_nested:
             if isinstance(element, Element) or element.accepts_multiple:
@@ -199,6 +209,7 @@ class ComplexType(AnyType):
                 element_value = value
                 child_path = list(render_path)
 
+            # We want to explicitly skip this sub-element
             if element_value is SkipValue:
                 continue
 
@@ -239,15 +250,9 @@ class ComplexType(AnyType):
         if isinstance(value, dict):
             return self(**value)
 
-        # Check if the valueclass only expects one value, in that case
-        # we can try to automatically create an object for it.
-        if len(self.attributes) + len(self.elements) == 1:
-            return self(value)
-
-        raise ValueError((
-            "Error while create XML for complexType '%s': "
-            "Expected instance of type %s, received %r instead."
-        ) % (self.qname or name, self._value_class, type(value)))
+        # Try to automatically create an object. This might fail if there
+        # are multiple required arguments.
+        return self(value)
 
     def resolve(self):
         """Resolve all sub elements and types"""
@@ -278,7 +283,7 @@ class ComplexType(AnyType):
         return self._resolved
 
     def extend(self, base):
-        """Create a new complextype instance which is the current type
+        """Create a new ComplexType instance which is the current type
         extending the given base type.
 
         Used for handling xsd:extension tags
@@ -337,6 +342,8 @@ class ComplexType(AnyType):
             attributes=attributes,
             qname=self.qname,
             is_global=self.is_global)
+
+        new._extension_types = base.accepted_types
         return new
 
     def restrict(self, base):

src/zeep/xsd/elements/any.py
@@ -181,9 +181,10 @@ class Any(Base):
         # Check if we received a proper value object. If we receive the wrong
         # type then return a nice error message
         if self.restrict:
-            expected_types = (etree._Element,) + self.restrict.accepted_types
+            expected_types = (etree._Element, dict,) + self.restrict.accepted_types
         else:
-            expected_types = (etree._Element, AnyObject)
+            expected_types = (etree._Element,  dict,AnyObject)
+
         if not isinstance(value, expected_types):
             type_names = [
                 '%s.%s' % (t.__module__, t.__name__) for t in expected_types
@@ -225,7 +226,7 @@ class AnyAttribute(Base):
         return self
 
     def render(self, parent, value, render_path=None):
-        if value is None:
+        if value in (None, NotSet):
             return
 
         for name, val in value.items():

src/zeep/xsd/types/base.py
@@ -15,6 +15,10 @@ class Type(object):
     def accept(self, value):
         raise NotImplementedError
 
+    @property
+    def accepted_types(self):
+        return tuple()
+
     def validate(self, value, required=False):
         return
 

tests/test_wsdl_arrays.py
@@ -1,12 +1,14 @@
 import io
 
+import pytest
 from lxml import etree
 
-from tests.utils import DummyTransport, assert_nodes_equal, load_xml
+from tests.utils import DummyTransport, assert_nodes_equal, load_xml, render_node
 from zeep import xsd
 
 
-def get_transport():
+@pytest.fixture(scope='function')
+def transport():
     transport = DummyTransport()
     transport.bind(
         'http://schemas.xmlsoap.org/soap/encoding/',
@@ -14,7 +16,7 @@ def get_transport():
     return transport
 
 
-def test_simple_type():
+def test_simple_type(transport):
     schema = xsd.Schema(load_xml("""
     <xsd:schema
         xmlns:xsd="http://www.w3.org/2001/XMLSchema"
@@ -30,7 +32,7 @@ def test_simple_type():
         </xsd:complexContent>
       </xsd:complexType>
     </xsd:schema>
-    """), transport=get_transport())
+    """), transport=transport)
 
     ArrayOfString = schema.get_type('ns0:ArrayOfString')
     print(ArrayOfString.__dict__)
@@ -53,7 +55,7 @@ def test_simple_type():
     assert_nodes_equal(expected, node)
 
 
-def test_complex_type():
+def test_complex_type(transport):
     schema = xsd.Schema(load_xml("""
     <xsd:schema
         xmlns:xsd="http://www.w3.org/2001/XMLSchema"
@@ -80,7 +82,7 @@ def test_complex_type():
         </xsd:complexContent>
       </xsd:complexType>
     </xsd:schema>
-    """), transport=get_transport())
+    """), transport=transport)
 
     ArrayOfObject = schema.get_type('ns0:ArrayOfObject')
     ArrayObject = schema.get_type('ns0:ArrayObject')
@@ -113,7 +115,7 @@ def test_complex_type():
     assert_nodes_equal(expected, node)
 
 
-def test_complex_type_without_name():
+def test_complex_type_without_name(transport):
     schema = xsd.Schema(load_xml("""
     <xsd:schema
         xmlns:xsd="http://www.w3.org/2001/XMLSchema"
@@ -137,7 +139,7 @@ def test_complex_type_without_name():
         </xsd:complexContent>
       </xsd:complexType>
     </xsd:schema>
-    """), transport=get_transport())
+    """), transport=transport)
 
     ArrayOfObject = schema.get_type('ns0:ArrayOfObject')
     ArrayObject = schema.get_type('ns0:ArrayObject')
@@ -370,3 +372,76 @@ def test_soap_array_parse():
     assert data.FlagDetailsStruct[0].Value == 'value1'
     assert data.FlagDetailsStruct[1].Name == 'flag2'
     assert data.FlagDetailsStruct[1].Value == 'value2'
+
+
+def test_xml_soap_enc_string(transport):
+    schema = xsd.Schema(load_xml("""
+        <?xml version="1.0"?>
+        <xsd:schema
+            xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+            xmlns:tns="http://tests.python-zeep.org/"
+            xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"
+            xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/"
+            targetNamespace="http://tests.python-zeep.org/"
+            elementFormDefault="qualified">
+          <xsd:import namespace="http://schemas.xmlsoap.org/soap/encoding/"/>
+
+          <xsd:element name="value" type="tns:ArrayOfString"/>
+
+          <xsd:complexType name="ArrayOfString">
+            <xsd:complexContent>
+              <xsd:restriction base="soapenc:Array">
+                <xsd:attribute ref="soapenc:arrayType" wsdl:arrayType="soapenc:string[]"/>
+              </xsd:restriction>
+            </xsd:complexContent>
+          </xsd:complexType>
+
+        </xsd:schema>
+    """), transport)
+    shoe_type = schema.get_element('{http://tests.python-zeep.org/}value')
+
+    obj = shoe_type(["foo"])
+    node = render_node(shoe_type, obj)
+    expected = """
+        <document>
+            <ns0:value xmlns:ns0="http://tests.python-zeep.org/">
+              <string>foo</string>
+            </ns0:value>
+        </document>
+    """
+    assert_nodes_equal(expected, node)
+
+    obj = shoe_type.parse(node[0], schema)
+    assert obj._value_1[0]['_value_1'] == "foo"
+
+    # Via string-types
+    string_type = schema.get_type('{http://schemas.xmlsoap.org/soap/encoding/}string')
+    obj = shoe_type([string_type('foo')])
+    node = render_node(shoe_type, obj)
+    expected = """
+        <document>
+            <ns0:value xmlns:ns0="http://tests.python-zeep.org/">
+              <string>foo</string>
+            </ns0:value>
+        </document>
+    """
+    assert_nodes_equal(expected, node)
+
+    obj = shoe_type.parse(node[0], schema)
+    assert obj._value_1[0]['_value_1'] == "foo"
+
+    # Via dicts
+    string_type = schema.get_type('{http://schemas.xmlsoap.org/soap/encoding/}string')
+    obj = shoe_type([{'_value_1': 'foo'}])
+    node = render_node(shoe_type, obj)
+    expected = """
+        <document>
+            <ns0:value xmlns:ns0="http://tests.python-zeep.org/">
+              <string>foo</string>
+            </ns0:value>
+        </document>
+    """
+    assert_nodes_equal(expected, node)
+
+    obj = shoe_type.parse(node[0], schema)
+    assert obj._value_1[0]['_value_1'] == "foo"

tests/test_xsd_extension.py
@@ -700,4 +700,3 @@ def test_restrict_on_ref():
 
     type_cls = schema.get_type('ns0:type')
     assert type_cls.signature()
-

commit c0172fdeb03295523fecf7b426c782facc31ec4c

fix: renderpath for type-extensions
The `child_path` variable can still be set from the attribute loop
above, but then it has the wrong value. Without any attributes it is
unset and causes an UnboundLocalError. Set it always to a proper value.

Fixes: 0c8a3fc4f9a8 ("Add support to pass base-types to type extensions")

src/zeep/xsd/types/complex.py
@@ -279,6 +279,7 @@ class ComplexType(AnyType):
             and not isinstance(value, (list, dict, CompoundValue))
         ):
             element = self.elements_nested[0][1]
+            child_path = render_path + [element.name]
             element.type.render(node, value, None, child_path)
             return
 

tests/test_xsd_complex_types.py
@@ -365,6 +365,39 @@ def test_xml_simple_content_nil():
     assert obj._value_1 is None
 
 
+def test_xml_simple_content_extension_render():
+    schema = xsd.Schema(
+        load_xml(
+            """
+    <?xml version="1.0"?>
+    <schema xmlns="http://www.w3.org/2001/XMLSchema"
+            xmlns:tns="http://tests.python-zeep.org/"
+            targetNamespace="http://tests.python-zeep.org/"
+            elementFormDefault="qualified">
+      <element name="container">
+        <complexType>
+          <simpleContent>
+            <extension base="string" />
+          </simpleContent>
+        </complexType>
+      </element>
+    </schema>
+    """
+        )
+    )
+    schema.set_ns_prefix("tns", "http://tests.python-zeep.org/")
+    container_elm = schema.get_element("tns:container")
+
+    result = render_node(container_elm, "my-test-value")
+
+    expected = """
+      <document>
+        <ns0:container xmlns:ns0="http://tests.python-zeep.org/">my-test-value</ns0:container>
+      </document>
+    """
+    assert_nodes_equal(result, expected)
+
+
 def test_ignore_sequence_order():
     schema_doc = load_xml(
         b"""